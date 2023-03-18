19060a05193877b049da5dd0cb4ac678f0fef607
Pravin Shirsath <p.shirsath@splendornet.com>
2023-03-18 12:18:07 +0530

    Created Build [18-03-2023-12.17pm]

--- build/static/js/214.e239c9f4.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpreactify=this.webpackJsonpreactify||[]).push([[214],{2403:function(t,e,a){"use strict";a.r(e),a.d(e,"default",(function(){return r}));a(1);var c=a(34),n=a(63),s=a(10),i=a(0);function r(t){return Object(i.jsxs)("div",{className:"blank-wrapper",children:[Object(i.jsxs)(c.a,{children:[Object(i.jsx)("title",{children:"Blank Page"}),Object(i.jsx)("meta",{name:"description",content:"Automaton Blank Page"})]}),Object(i.jsx)(n.a,{title:Object(i.jsx)(s.a,{id:"sidebar.blank"}),match:t.match})]})}}}]);
-//# sourceMappingURL=214.e239c9f4.chunk.js.map
