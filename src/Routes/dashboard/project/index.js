a49e53d3b8eeb075f15b2894463c914e8f8f5088
Vishal <v.chauhan.splendornet.com>
2023-04-13 12:51:18 +0530

    1. card height and image resized 2. Navigation user block avatar resized 3. Notification icon resized

--- src/Routes/dashboard/project/index.js
+++ src/Routes/dashboard/project/index.js
@@ -267,25 +267,25 @@ export default function SaasDashbaord(props) {
                   key={i}
                 >
                   <div className="shadow project-card-shadow">
-                    <section className="dark-primary text-white px-3 shadow rounded d-flex  align-items-center ">
+                    <section className="dark-primary text-white px-3 shadow rounded d-flex  align-items-center" style={{borderColor:'white', height:"90px"}}>
 
                       <img
                        
                         src={item?.project_image == null ? `${process.env.PUBLIC_URL}/assets/images/avatars/user-15.jpg` : `${ item.project_image}`}
                         alt="user profile"
                         className=" rounded-circle "
-                        width={50}
-                        height={50}
+                        width={63}
+                        height={63}
                       />
 
-                      <div className="ml-4  align-items-center ">
-                        <p className="project-heading">{item.project_name}</p>
-                        <p className="project-date"> Created by {item.username} on {created}</p>
+                      <div className="ml-4  d-flex flex-column justify-content-center align-items-center">
+                        <p className="project-heading text-left w-100 m-0">{item.project_name}</p>
+                        <p className="project-date text-left w-100 m-0"> Created by {item.username} on {created}</p>
                       </div>
 
 
                     </section>
-                    <section className="Project-Card-Main ">
+                    <section className="Project-Card-Main" style={{height: "195px"}}>
                       <section className="d-flex flex-direction: column align-items-center justify-content-between Project-cart-main">
                         <div className="Doughnut-in-Project-cart">
                           <DoughnutChart />
@@ -296,7 +296,7 @@ export default function SaasDashbaord(props) {
                         </div>
 
                       </section>
-                      <section className="d-flex flex-direction: column align-items-center justify-content-center   " >
+                      <section className="d-flex flex-direction: column align-items-center justify-content-center"  style={{marginTop:"18px"}} >
                         <Button variant="contained" color="primary" className="projectCardButton mx-2" onClick={()=>EditModal(item)} >Edit</Button>
                         <Button variant="contained" color="primary" className="projectCardButton mx-2" onClick={()=>ViewProject(item)} >View</Button>
                         <Button variant="contained" color="danger" className="projectCardButton mx-2" onClick={()=>DeletModalOpen(item)} >Delete</Button>
