9d526aa322f13c3c8163ad449a5aab35073c504f
Pravin Shirsath <p.shirsath@splendornet.com>
2023-04-21 00:21:17 +0530

    created build 21 april

---
+++ build/static/js/216.70a58c5f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreactify=this.webpackJsonpreactify||[]).push([[216],{2584:function(t,c,e){"use strict";e.r(c);e(1);var i=e(0);c.default=function(){return Object(i.jsx)("div",{children:"Account"})}}}]);
+//# sourceMappingURL=216.70a58c5f.chunk.js.map
---
+++ build/static/js/217.69cd85a1.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreactify=this.webpackJsonpreactify||[]).push([[217],{2586:function(t,c,e){"use strict";e.r(c);e(1);var i=e(0);c.default=function(){return Object(i.jsx)("div",{children:Object(i.jsx)("h1",{children:" Project  "})})}}}]);
+//# sourceMappingURL=217.69cd85a1.chunk.js.map
