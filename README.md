6bb8283872c5f1b57a675b252d17d42eb25ee117
Pravin Shirsath <p.shirsath@splendornet.com>
2023-04-26 10:50:24 +0530

    build created

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Automaton
+# Hello Geo
 
--- build/static/js/209.79785e9d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpreactify=this.webpackJsonpreactify||[]).push([[209],{2562:function(t,c,e){"use strict";e.r(c);e(1);var i=e(0);c.default=function(){return Object(i.jsx)("div",{children:"Account"})}}}]);
-//# sourceMappingURL=209.79785e9d.chunk.js.map
